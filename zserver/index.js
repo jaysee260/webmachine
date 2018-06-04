3a774ece05e5f77888a5c16970f60fadec7a8909
pdhoward <patrick.howard@hotmail.com>
2018-06-04 11:55:31 -0400

    synch

--- zserver/index.js
+++ zserver/index.js
@@ -27,7 +27,7 @@ const app =   express();;
 let envState = true
 if ( process.env.isLive == 'false' ) {
     envState = false
-    require('../db/mongoose')(envState)
+    require('../db/mongoose')(envState)  
 }
 
 
