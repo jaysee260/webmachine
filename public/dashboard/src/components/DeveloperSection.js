2eb7bcfd1c45489e9d3670ef04f4679ffb1e0b4f
jc <javascriptdeveloper17@gmail.com>
2018-06-30 21:20:52 -0400

    wip commit

--- public/dashboard/src/components/DeveloperSection.js
+++ public/dashboard/src/components/DeveloperSection.js
@@ -2,8 +2,19 @@ import React from "react";
 
 export const DeveloperSection = ({ content }) => 
   <section>
-    <h1>{ content.title }</h1>
-    <h4>{ content.noSubscriptions }</h4>
+    {/*
+      In essence... This section needs to render
+      a set of content if the person is a developer
+      for AT LEAST one network, and another set of
+      content if the person is not a developer for
+      ANY network.
+
+      The button to browse networks that support a
+      marketplace should always render.
+    */}
+    <h1>{ content.isNotDeveloper.title }</h1>
+    <h4>{ content.isNotDeveloper.subtitle }</h4>
+    {/* <h4>{ content.isNotDeveloper.description }</h4> */}
 
     <div>
       <a href="#" className="btn btn-default">
