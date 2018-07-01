f8b6f1fc0675c846132f5227386c2995c928cc0f
jc <javascriptdeveloper17@gmail.com>
2018-07-01 17:43:31 -0400

    setting up page for developer registration

---
+++ public/developer/src/DeveloperRegistration.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Auth from '../../home/src/Pages/Auth/Auth'
+
+class DeveloperRegistration extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount() {
+    let auth = new Auth();
+    if (!auth.isAuthenticated())
+      auth.login()
+  }
+
+  render() {
+    return (
+      <h1>Developer Registration Page</h1>
+    )
+  }
+}
+
+export default DeveloperRegistration;
