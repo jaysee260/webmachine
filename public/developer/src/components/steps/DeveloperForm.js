a14945a1725e811159e40599a5560ad0c642ab52
jc <javascriptdeveloper17@gmail.com>
2018-07-04 12:25:57 -0400

    initiated communication with backend

---
+++ public/developer/src/components/steps/DeveloperForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class DeveloperForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  async componentWillMount() {
+    
+  }
+
+}
+
+export default DeveloperForm
